57651ebb4ae319264a4d5589c11e40ffa0176f05
James Trimble <james.trimble@yahoo.co.uk>
2021-01-09 17:07:38 +0000

    Delete old verify script

--- verify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat instance-lists/instances.txt | while read family i; do
-	cat program-output/$i-* | grep -E 'Depth|final treedepth' \
-		| awk '/Depth/ {print $2} /final treedepth/ {print $4}' \
-		| sed 's/.*-//' \
-		| tr -d ']' \
-		| sort | uniq -c | wc -l
-done | sort | uniq -c
